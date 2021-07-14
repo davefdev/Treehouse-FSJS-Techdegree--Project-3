59a3cf8cab9720f0a456bf870b791955d9b7c97f
Davef <76170782+davefdev@users.noreply.github.com>
2021-07-15 00:53:43 +0100

    Add files via upload

---
+++ Treehouse-FSJS-Techdegree-Project-3/script.js
@@ -0,0 +1,13 @@
+/*
+Treehouse Techdegree:
+FSJS Project 3 - Interactive Form
+*/
+
+
+//focus on the name input field
+const focusName = document.querySelectorAll('input[type=text]');
+console.log(focusName);
+focusName[0].focus();
+
+//job role 
+const jobRoleSelect = document.querySelector('#title');
