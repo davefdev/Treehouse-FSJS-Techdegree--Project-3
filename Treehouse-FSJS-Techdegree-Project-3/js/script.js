a5d828829a03ddf34d39b277e5238a14e1b13089
Davef <76170782+davefdev@users.noreply.github.com>
2021-07-19 00:10:58 +0100

    Add files via upload

--- Treehouse-FSJS-Techdegree-Project-3/js/script.js
+++ Treehouse-FSJS-Techdegree-Project-3/js/script.js
@@ -9,5 +9,22 @@ const focusName = document.querySelectorAll('input[type=text]');
 console.log(focusName);
 focusName[0].focus();
 
-//job role 
+//access job role 
 const jobRoleSelect = document.querySelector('#title');
+const otherJobRoleInput = document.querySelector('#other-job-role')
+
+//create a hide element function that can be reused
+const hideElement(elem) {
+    elem.style.display = 'hidden';
+}
+//hide the other job element
+hideElement(otherJobRoleInput);
+
+//event listener on change to hide other job role
+jobRoleSelect.addEventListener('change', () => {
+    if (jobRoleSelect.value === 'other') {
+        displayElement(otherJobRoleInput, 'block');
+    } else {
+        hideElement(otherJobRoleInput);
+    }
+}); 
